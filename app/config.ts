cf26861c88f3ae72c9d6ace86c8b8bf39b8fffa5
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-27 00:35:49 +0900

    feat: update contract address

--- app/config.ts
+++ app/config.ts
@@ -2,7 +2,7 @@ export const NEXT_PUBLIC_URL = "https://recast-relay-syndicate.vercel.app/";
 
 export const ALLOWED_ORIGIN = "recast-relay-syndicate.vercel.app";
 
-export const NFT_ADDRESS = "0xf74661d961f16807ebb96aeff60c63afcd5a005f";
+export const NFT_ADDRESS = "0xcc967099014d26c5d27f77351cfa1f70d832a646";
 
 export const CARD_DIMENSIONS = {
   width: 800,
