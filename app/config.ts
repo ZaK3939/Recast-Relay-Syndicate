cd68feb7acbb5c7cebf8f322232c5982e450f786
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-27 00:33:51 +0900

    fix: Arrowed origin

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
-export const NEXT_PUBLIC_URL = "https://syndicate-nft-recast.vercel.app/";
+export const NEXT_PUBLIC_URL = "https://recast-relay-syndicate.vercel.app/";
 
-export const ALLOWED_ORIGIN = "syndicate-nft-recast.vercel.app";
+export const ALLOWED_ORIGIN = "recast-relay-syndicate.vercel.app";
 
 export const NFT_ADDRESS = "0xf74661d961f16807ebb96aeff60c63afcd5a005f";
 
